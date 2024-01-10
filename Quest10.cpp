daee941398675c2cac5a2f779aac86ab587d600d
Sahan0312 <155511001+Sahan0312@users.noreply.github.com>
2024-01-10 11:16:16 +0530

    add Private Function

--- Quest10.cpp
+++ Quest10.cpp
@@ -16,6 +16,87 @@ private:
 public:
 
 
+private:
+    void initializeBoard() {
+        board.assign(rows, vector<char>(cols, ' '));
+        mineLocations.assign(rows, vector<bool>(cols, false));
+    }
+
+    void placeMines() {
+        srand(static_cast<unsigned>(time(nullptr)));
+        int minesPlaced = 0;
+
+        while (minesPlaced < totalMines) {
+            int randRow = rand() % rows;
+            int randCol = rand() % cols;
+
+            if (!mineLocations[randRow][randCol]) {
+                mineLocations[randRow][randCol] = true;
+                minesPlaced++;
+            }
+        }
+    }
+
+    void calculateNumbers() {
+        for (int i = 0; i < rows; ++i) {
+            for (int j = 0; j < cols; ++j) {
+                if (!mineLocations[i][j]) {
+                    int count = countAdjacentMines(i, j);
+                    if (count > 0) {
+                        board[i][j] = static_cast<char>('0' + count);
+                    }
+                }
+            }
+        }
+    }
+
+    int countAdjacentMines(int row, int col) const {
+        int count = 0;
+        for (int i = -1; i <= 1; ++i) {
+            for (int j = -1; j <= 1; ++j) {
+                int newRow = row + i;
+                int newCol = col + j;
+                if (newRow >= 0 && newRow < rows && newCol >= 0 && newCol < cols &&
+                    mineLocations[newRow][newCol]) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+    void revealCell(int row, int col) {
+        if (row < 0 || row >= rows || col < 0 || col >= cols || board[row][col] != ' ') {
+            return;
+        }
+
+        int adjacentMines = countAdjacentMines(row, col);
+        board[row][col] = (adjacentMines == 0) ? ' ' : static_cast<char>('0' + adjacentMines);
+
+        if (adjacentMines == 0) {
+            for (int i = -1; i <= 1; ++i) {
+                for (int j = -1; j <= 1; ++j) {
+                    revealCell(row + i, col + j);
+                }
+            }
+        }
+    }
+
+    bool checkWin() const {
+        for (int i = 0; i < rows; ++i) {
+            for (int j = 0; j < cols; ++j) {
+                if (!mineLocations[i][j] && board[i][j] == ' ') {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+
+
+
+
 };
 
 int main() {
