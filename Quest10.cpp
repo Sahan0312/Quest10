a9c28ce75a0ebaa54ace4f76291c23d662b75b20
Daham Adikari <dahamadikary0712@gmail.com>
2024-01-10 11:22:06 +0530

    Update Quest10.cpp

--- Quest10.cpp
+++ Quest10.cpp
@@ -94,6 +94,41 @@ private:
     }
 
 
+public:
+    Minesweeper(int rows, int cols, int mines) : rows(rows), cols(cols), totalMines(mines) {
+        initializeBoard();
+        placeMines();
+        calculateNumbers();
+    }
+
+    void printBoard(bool showMines = false) const {
+        for (int i = 0; i < rows; ++i) {
+            for (int j = 0; j < cols; ++j) {
+                if (mineLocations[i][j] && showMines) {
+                    cout << "* ";
+                }
+                else {
+                    cout << board[i][j] << " ";
+                }
+            }
+            cout << endl;
+        }
+    }
+
+    bool play(int row, int col) {
+        if (mineLocations[row][col]) {
+            cout << "\nGame Over! You hit a mine." << endl;
+            return false;
+        }
+        else {
+            revealCell(row, col);
+            if (checkWin()) {
+                cout << "\nCongratulations! You won!" << endl;
+                return false;
+            }
+            return true;
+        }
+    }
 
 
 
@@ -104,5 +139,23 @@ int main() {
 
     cout << "Enter the number of rows, columns, and mines: ";
     cin >> rows >> cols >> mines;
+    
+    Minesweeper game(rows, cols, mines);
+
+    while (true)
+    {
+        int row, col;
+
+        cout << "Enter row and column (starting from 0) to reveal: ";
+        cin >> row >> col;
+        game.printBoard();
+        if (!game.play(row, col)) {
+            break;
+        }
+    }
+
+    game.printBoard(true);
+
+    
     return 0;
 }
